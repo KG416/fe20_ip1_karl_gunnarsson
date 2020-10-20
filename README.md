972fe05e30d2a92844b77d20dcd28452105f354c
karlG <kallegunnarsson@hotmail.com>
2020-10-20 12:27:08 +0200

    Fixed sizing issue with img+vid media queries. Added border radius to img

--- README.md
+++ README.md
@@ -1,17 +1,1 @@
 # fe20_ip1_karl_gunnarsson
-kyh-uppgift: Individuella 1
-
-TO DO:
-
-----------EXTRA STUFF----------
-
-2. Gör allt lite större för mobil
-
-3. Starta video när man kommer ner till den via scroll
-document.getElementById('vid').play();
-
-4. Internt IP + port i browsern
-Mobile safari, kolla blåfärgen
-
-Lösning:
-https://stackoverflow.com/questions/42507623/blue-highlight-on-onclick-on-buttons-in-chrome-mobile
--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-// Form validation START
-//Visar felmeddelande om man submittar utan
-//att skriva något namn
+//Form validation START
+//Shows red error message if user submits without name or email
 
-// Källa: Net Ninja Simple form validation
-// Länk: https://www.youtube.com/watch?v=b0pxAb_yy2U&list=PL4cUxeGkcC9i9Ae2D9Ee1RvylH38dKuET&index=45
+// Source: Net Ninja Simple Form Validation
+// https://www.youtube.com/watch?v=b0pxAb_yy2U&list=PL4cUxeGkcC9i9Ae2D9Ee1RvylH38dKuET&index=45
 
 var form = document.forms.form;
 var message = document.getElementById("message");
 
 form.onsubmit = function () {
-
     if (form.username.value === "") {
         message.innerHTML = "Please enter a name before you send request";
         return false;
@@ -21,6 +19,5 @@ form.onsubmit = function () {
         message.innerHTML = "";
         return true;
     }
-
 };
 // Form validation SLUT
